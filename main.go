d1b8417bea121c93dc03b75265925236136ce06c
JP Hastings-Spital <jp@deliveroo.co.uk>
2022-10-10 12:37:18 +0100

    Unsure?

--- main.go
+++ main.go
@@ -30,17 +30,16 @@ func main() {
 	input := os.Args[1]
 	output := strings.TrimSuffix(input, filepath.Ext(input)) + ".dither.png"
 
+	var pPal color.Palette
+	if len(os.Args) >= 3 && os.Args[2][0] == '#' {
+		pPal = parsePalette(os.Args[2])
+	}
+
 	forceBlack := false
 	forceWhite := false
-	hX := 1.0
-	_ = hX
-	cX := 1.5
-	_ = cX
-	lX := 1.0
-	_ = lX
 	dX := uint(16)
 	_ = dX
-	sX := float32(0.64)
+	sX := float32(0.25)
 	_ = sX
 	k := 8
 
@@ -58,6 +57,84 @@ func main() {
 	img = resize.Thumbnail(512, 512, img, resize.Lanczos3)
 	bb := img.Bounds()
 
+	if pPal == nil {
+		pPal = makePalette(img, k, forceBlack, forceWhite)
+	}
+
+	d := dither.NewDitherer(pPal)
+	//d.Mapper = dither.RandomNoiseGrayscale(-0.5, 0.5)
+	d.Mapper = dither.Bayer(dX, dX, sX) // Why not?
+	//d.Mapper = dither.PixelMapperFromMatrix(dither.ClusteredDot4x4, sX)
+	//d.Matrix = dither.FloydSteinberg
+	//d.Matrix = dither.Atkinson
+
+	// Open an image and save it as a dithered GIF
+
+	imgd := d.Dither(img)
+	height := bb.Max.Y
+	width := bb.Max.X
+	landscape := width > height
+	if landscape {
+		bb.Max.Y = width
+	} else {
+		bb.Max.X = height
+	}
+
+	img2 := image.NewPaletted(bb, pPal)
+	draw.Draw(img2, img2.Bounds(), imgd, image.Point{}, draw.Src)
+
+	sizes := make(map[color.Color]int, len(pPal))
+	for x := 0; x < width; x++ {
+		for y := 0; y < height; y++ {
+			sizes[img2.At(x, y)]++
+		}
+	}
+
+	pos := 0
+	for _, col := range pPal {
+		size := float64(sizes[col]) / float64(width * height)
+
+		var bbb image.Rectangle
+		if landscape {
+			blockW := int(size * float64(width))
+			bbb = image.Rect(pos, height, pos + blockW, width)
+			pos += blockW
+		} else {
+			blockH := int(size * float64(height))
+			bbb = image.Rect(width, pos, height, pos+blockH)
+			pos += blockH
+		}
+		draw.Draw(img2, bbb, &image.Uniform{C: col}, image.Point{}, draw.Src)
+	}
+
+	f3, err := os.Create(output)
+	if err != nil {
+		panic(err)
+	}
+
+	err = png.Encode(f3, img2)
+	if err != nil {
+		panic(err)
+	}
+}
+
+func parsePalette(str string) (pPal color.Palette) {
+	for _, hex := range strings.Split(str[1:], "#") {
+		var c color.RGBA
+		c.A = 0xff
+		if _, err := fmt.Sscanf(hex, "%02x%02x%02x", &c.R, &c.G, &c.B); err != nil {
+			panic(err)
+		}
+		pPal = append(pPal, c)
+	}
+	return pPal
+}
+
+func makePalette(img image.Image, k int, forceBlack, forceWhite bool) color.Palette {
+	hX := 1.0
+	cX := 1.5
+	lX := 1.0
+
 	if forceBlack {
 		k--
 	}
@@ -68,7 +145,7 @@ func main() {
 
 	cols, err := prominentcolor.KmeansWithAll(k, img,
 		prominentcolor.ArgumentLAB|prominentcolor.ArgumentNoCropping,
-		128, prominentcolor.GetDefaultMasks())
+		512, nil)
 	if err != nil {
 		panic(err)
 	}
@@ -102,53 +179,15 @@ func main() {
 		cPal = append(cPal, w)
 	}
 
+	fmt.Println("Colour string:")
 	var pPal color.Palette
 	for _, c := range sortColors(cPal) {
 		pPal = append(pPal, c)
+		fmt.Printf(c.Hex())
 	}
+	fmt.Println()
 
-	d := dither.NewDitherer(pPal)
-	d.Mapper = dither.Bayer(dX, dX, sX) // Why not?
-	//d.Mapper = dither.PixelMapperFromMatrix(dither.ClusteredDot4x4, sX)
-	//d.Matrix = dither.FloydSteinberg
-	//d.Matrix = dither.Atkinson
-
-	// Open an image and save it as a dithered GIF
-
-	imgd := d.Dither(img)
-	height := bb.Max.Y
-	width := bb.Max.X
-	landscape := width > height
-	if landscape {
-		bb.Max.Y = width
-	} else {
-		bb.Max.X = height
-	}
-
-	img2 := image.NewPaletted(bb, pPal)
-	draw.Draw(img2, img2.Bounds(), imgd, image.Point{}, draw.Src)
-
-	blockW := width / k
-	blockH := height / k
-	for i, col := range pPal {
-		var bbb image.Rectangle
-		if landscape {
-			bbb = image.Rect(i*blockW, height, (i+1)*blockW, width)
-		} else {
-			bbb = image.Rect(width, i*blockH, height, (i+1)*blockH)
-		}
-		draw.Draw(img2, bbb, &image.Uniform{C: col}, image.Point{}, draw.Src)
-	}
-
-	f3, err := os.Create(output)
-	if err != nil {
-		panic(err)
-	}
-
-	err = png.Encode(f3, img2)
-	if err != nil {
-		panic(err)
-	}
+	return pPal
 }
 
 func sortColors(cs1 []colorful.Color) []colorful.Color {
